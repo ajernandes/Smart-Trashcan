7ab16faec702781cb05f1d865e3b8633ae339eb4
AJ Ernandes <aj.ernandes01@gmail.com>
2021-03-31 14:46:03 -0400

    it works!

--- client.py
+++ client.py
@@ -1,4 +1,14 @@
 import base64
+from websocket import create_connection
+ws = create_connection("ws://localhost:8080")
+with open("image.jpeg", "rb") as img_file:
+    encoded_string = base64.b64encode(img_file.read())
+    ws.send(encoded_string)
+    print("Sent")
+    print("Receiving...")
+    result =  ws.recv()
+    print("Received '%s'" % result)
+    ws.close()
 
-with open("image.jpeg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
+
+
